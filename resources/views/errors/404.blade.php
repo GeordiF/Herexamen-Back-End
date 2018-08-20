d8d0c28b9232d29494e6015a374838ede3a9746f
GeordiF <geordi.feijens@student.kdg.be>
2018-08-20 15:12:53 +0200

    404 and 500 page created

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.error')
+
+@section('content')
+
+<h2>404 PAge not found!</h2>
+
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.error')
+
+@section('content')
+
+<h2>500 internal server error, refresh in a few moments!</h2>
+
+@endsection
