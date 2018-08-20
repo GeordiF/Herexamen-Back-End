a1015ddc8bf35df4200b4043379766eae7651fcd
GeordiF <geordi.feijens@student.kdg.be>
2018-08-20 15:56:01 +0200

    button on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,6 @@
 
 <p>You are logged in!</p>
 
+<a href="{{ url('/cards') }}" class="btn btn-primary">Go to cards</a>
+
 @endsection
--- resources/views/test.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<h1>This is a test</h1>
-
-@endsection
